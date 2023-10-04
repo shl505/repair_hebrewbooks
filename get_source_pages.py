cbbc395f53df1c410080827e9fdd15f202754471
שלוימי <136237874+shl505@users.noreply.github.com>
2023-10-04 13:39:50 +0300

    Update get_source_pages.py

--- get_source_pages.py
+++ get_source_pages.py
@@ -2,6 +2,7 @@ import pywikibot
 
 #הגדרת פונקציה
 def get_page_text(page_title):
+    """פונקציה שמקבלת שם של דף במכלול ומחזירה את קוד המקור שלו"""
     site = pywikibot.Site(url="https://www.hamichlol.org.il/") # הגדרת המכלול
     page = pywikibot.Page(site, title=page_title) #קבלת הדף
     return page.text
