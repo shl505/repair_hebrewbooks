d7d6242d097e1438f9c051e120e579c24c2056c6
EMN308 <95176947+EMN308@users.noreply.github.com>
2023-10-04 19:24:49 +0300

    קובץ פונקציית הבוט

--- bot_main.py
+++ bot_main.py
@@ -1,23 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-import pywikibot
 from repair_text import repair_text
+import mwclient
+from mwclient import Site
+from mwclient import page
 
 # המחרוזת שבה אתה מחפש
-search_term = "www.hebrewbooks.org"
+search_term = "hebrewbooks.org"
 
 # ה-URL שבו מופיע המידע
 url = f"https://www.hamichlol.org.il/w/index.php?title=%D7%9E%D7%99%D7%95%D7%97%D7%93:%D7%97%D7%99%D7%A4%D7%95%D7%A9_%D7%A7%D7%99%D7%A9%D7%95%D7%A8%D7%99%D7%9D_%D7%97%D7%99%D7%A6%D7%95%D7%A0%D7%99%D7%99%D7%9D&limit=2&offset=0&target={search_term}&namespace=0"
 def bot(url:str):
     # שליחת בקשת HTTP GET ל-URL
     response = requests.get(url)
-
     # יצירת מבנה ה-BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # חיפוש על פי התגית "li" שמכילה את התוצאות
-    results = soup.find_all('li')
-
+    results = soup.find_all('li')[1:]
+    site = Site("www.hamichlol.org.il")
+    site.login("שלוימי", "")
     for result in results:
         # בדיקה אם התוכן שבתוך ה-<li> מכיל את ה-search_term
         if search_term in result.text:  # חיפוש והשגת הלינק
@@ -27,24 +29,24 @@ def bot(url:str):
             if link:
                 # השגת הערך של ה-`title`
                 title = link['title']
-
+                title = "הגט מקליווא"
                 # Connect to Wikipedia using pywikibot
-                site = pywikibot.Site(url="https://www.hamichlol.org.il/")
-                site.login(site, 'שלוימי בוט', 'סיסמתך')
-                strings = ["[http://www.hebrewbooks.org/", "[http://hebrewbooks.org/"]
+
+
                 # Loop through each page title
-                for i in title:
-                    # Get the Wikipedia page object
-                    page = pywikibot.Page(site, i)
-                    page_text = page.text
 
-                    # Perform your edits, add new text, etc.
-                    # For example, adding a new line at the end of the page
-                    new_text = page_text + repair_text(i, strings)
+                # Get the Wikipedia page object
+                page = mwclient.page.Page(site, title)
+                page_text = page.text()
+                print(page_text)
 
-                    # Save the updated page
-                    page.text = new_text
-                    page.save("המרת קישורי היברובוקס לתבניות")
+                # Perform your edits, add new text, etc.
+                # For example, adding a new line at the end of the page
+                new_text = repair_text(page_text)
+
+                # Save the updated page
+                page.save(new_text)
 
                 # Disconnect from Wikipedia
-                site.logout()
+
+bot(url)
