66e2aff186feacd492464a0bc56e378170c9211e
שלוימי <136237874+shl505@users.noreply.github.com>
2023-10-04 12:29:47 +0300

    הערות

--- hybro_links_to_template.py
+++ hybro_links_to_template.py
@@ -3,13 +3,21 @@ import re
 class Page:
 
     def __init__(self, page_text):
-        self.page_text = page_text #קבלת טקסט המקור של הדף
+        """פונקציה שמקבלת שם דף מחזירה קוד מקור של דף במכלול"""
+        self.page_text = page_text
 
+    #--------------------------------------------------------------------------------#
 
     def get_list_hybro_links(self):
+        """פונקציה שמקבלת מחרוזת ומחזירה רשימה של כל הקישורים לאתר היברובוקס במחרוזת"""
+        #הגדרת משתנה שמכיל מחרוזת רגולרית
         link_tamplate = r"\[http://(?:www\.)?hebrewbooks\.org/\d+\s\w+(?:\s\w+)*\]"
+        #חיפוש המחרוזת הרגולרית והכנסת התוצאות לרשימה
         list_links = re.findall(link_tamplate, self.page_text)
+        #החזרת הרשימה
         return list_links
+    
+    #--------------------------------------------------------------------------------#
 
 page = Page(page_text="""
 * '''פירוש רבנו חננאל''' על התלמוד, למסכתות: [http://hebrewbooks.org/34550 פסחים]
