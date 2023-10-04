8083e13ae570e2521c224ee54d45360b3595407a
shl505 <1912081@gmail.com>
2023-10-04 11:01:25 +0300

    שימוש בלולאה

--- repair_text2.py
+++ repair_text2.py
@@ -1,34 +1,35 @@
 from get_source_pages import get_page_text
 
-strings = ["[http://www.hebrewbooks.org/", "[http://hebrewbooks.org/" ]
 
-def repair_text(repaired_text: str, strings: list):
+
+def repair_text(repaired_text: str):
+    #הגדרת משתנה שמכיל את הטקסט המקורי
+    parts = repaired_text
+    #הגדרת משתנה מטיפוס רשימה שמכיל את ראשי הקישורים
+    strings = ["[http://www.hebrewbooks.org/", "[http://hebrewbooks.org/"]
+    #הגדרת משתנה שמכיל את ראש התבנית
+    string_repair = "{{היברובוקס||"
     for string in strings:
-        string_repair = "{{היברובוקס||"
         if string and repaired_text.find(string) >= 0:
             number = repaired_text.count(string)
 
             for i in range(number):
                 parts = repaired_text
-
-                if "pagefeed/hebrewbooks_org" in parts:
-                    if "[http://www.hebrewbooks.org/pagefeed/hebrewbooks" in parts:
-                        list1 = parts.split("[http://www.hebrewbooks.org/pagefeed/hebrewbooks", 1)
-                        repair_pagefeed = list1[1].split("_", 3)
-                        page_number = repair_pagefeed[3].split(".pdf", 1)
-                        name = page_number[1].split("]", 1)
-                        if name[0].count("[") == 0:
-                            repaired_text = list1[0] + string_repair + name[0][1:] + "|" + repair_pagefeed[2] + "||ללא|עמוד=" + page_number[0] + "}}" + name[1]
-                    elif "[http://hebrewbooks.org/pagefeed/hebrewbooks" in parts:
-                        list1 = parts.split("[http://hebrewbooks.org/pagefeed/hebrewbooks", 1)
-                        repair_pagefeed = list1[1].split("_", 3)
-                        page_number = repair_pagefeed[3].split(".pdf", 1)
-                        name = page_number[1].split("]", 1)
-                        if name[0].count("[") == 0:
-                            repaired_text = list1[0] + string_repair + name[0][1:] + "|" + repair_pagefeed[2] + "||ללא|עמוד=" + page_number[0] + "}}" + name[1]
+                pagefeed = "pagefeed/hebrewbooks"
+                if string + pagefeed in parts:
+                    list1 = parts.split(string + pagefeed, 1)
+                    repair_pagefeed = list1[1].split("_", 3)
+                    page_number = repair_pagefeed[3].split(".pdf", 1)
+                    name = page_number[1].split("]", 1)
+                    if name[0].count("[") == 0:
+                        repaired_text = list1[0] + string_repair + name[0][1:] + "|" + repair_pagefeed[2] + "||ללא|עמוד=" + page_number[0] + "}}" + name[1]
+                #בדיקה אם יש קישור מטיפוס pdfpager בטקסט
                 elif "pdfpager.aspx" in parts:
-                    if "[http://www.hebrewbooks.org/pdfpager.aspx?req=" in parts:
-                        repair_pdfpager = parts.split("[http://www.hebrewbooks.org/pdfpager.aspx?req=", 1)
+                    #הגדרת משתנים שמכילים את טיפוסי הקישורים האפשריים בpdfpager
+                    pdfpager_req = "pdfpager.aspx?req="
+                    pdfpager_sits = "pdfpager.aspx?sits"
+                    if string + pdfpager_req in parts:
+                        repair_pdfpager = parts.split(string + pdfpager_req, 1)
                         if "&pgnum=" in repair_pdfpager[1]:
                             repair_page = repair_pdfpager[1].split("&pgnum=", 1)  # חיתוך לפני מיקום מספר העמוד
                             name = repair_page[1].split(" ", 1)
@@ -60,41 +61,9 @@ def repair_text(repaired_text: str, strings: list):
                                 name[0] = repair_hilite[0]
                             if caption[0].count("[") == 0:
                                 repaired_text = repair_pdfpager[0] + string_repair + caption[0] + "|" + name[0] + "||ללא}}" + caption[1]
-                    elif "[http://hebrewbooks.org/pdfpager.aspx?req=" in parts:
-                        repair_pdfpager = parts.split("[http://hebrewbooks.org/pdfpager.aspx?req=", 1)
-                        if "&pgnum=" in repair_pdfpager[1]:
-                            repair_page = repair_pdfpager[1].split("&pgnum=", 1)  # חיתוך לפני מיקום מספר העמוד
-                            name = repair_page[1].split(" ", 1)
-                            caption = name[1].split("]", 1)
-                            if "&st=" in repair_page[0]:
-                                repair_st = repair_page[0].split("&st=", 1)
-                                if "&hilite=" in repair_st[0]:
-                                    repair_hilite = repair_st[0].split("&hilite=", 1)
-                                    repair_page[0] = repair_hilite[0]
-                                else:
-                                    repair_page[0] = repair_st[0]
-                            if "&hilite=" in name[0]:
-                                repair_hilite = name[0].split("&hilite=", 1)
-                                name[0] = repair_hilite[0]
-                            if caption[0].count("[") == 0:
-                                repaired_text = repair_pdfpager[0] + string_repair + caption[0] + "|" + repair_page[0] + "||ללא|עמוד=" + name[0] + "}}" + caption[1]
-                        else:
-                            name = repair_pdfpager[1].split(" ", 1)
-                            caption = name[1].split("]", 1)
-                            if "&st=" in name[0]:
-                                repair_st = name[0].split("&st=", 1)
-                                if "&hilite=" in repair_st[0]:
-                                    repair_hilite = repair_st[0].split("&hilite=", 1)
-                                    name[0] = repair_hilite[0]
-                                else:
-                                    name[0] = repair_st[0]
-                            if "&hilite=" in name[0]:
-                                repair_hilite = name[0].split("&hilite=", 1)
-                                name[0] = repair_hilite[0]
-                            if caption[0].count("[") == 0:
-                                repaired_text = repair_pdfpager[0] + string_repair + caption[0] + "|" + name[0] + "||ללא}}" + caption[1]
-                    elif "[http://hebrewbooks.org/pdfpager.aspx?sits" in parts:
-                        repair_sits = parts.split("[http://hebrewbooks.org/pdfpager.aspx?sits", 1)
+
+                    elif string + pdfpager_sits in parts:
+                        repair_sits = parts.split(string + pdfpager_sits, 1)
                         repair_pdfpager = repair_sits[1].split("&req=", 1)
                         if "&pgnum=" in repair_pdfpager[1]:
                             repair_page = repair_pdfpager[1].split("&pgnum=", 1)  # חיתוך לפני מיקום מספר העמוד
@@ -128,60 +97,19 @@ def repair_text(repaired_text: str, strings: list):
                             if caption[0].count("[") == 0:
                                 repaired_text = repair_sits[0] + string_repair + caption[0] + "|" + name[0] + "||ללא}}" + caption[1]
 
-                    elif "[http://www.hebrewbooks.org/pdfpager.aspx?sits" in parts:
-                        repair_sits = parts.split("[http://www.hebrewbooks.org/pdfpager.aspx?sits", 1)
-                        repair_pdfpager = repair_sits[1].split("&req=", 1)
-                        if "&pgnum=" in repair_pdfpager[1]:
-                            repair_page = repair_pdfpager[1].split("&pgnum=", 1)  # חיתוך לפני מיקום מספר העמוד
-                            name = repair_page[1].split(" ", 1)
-                            caption = name[1].split("]", 1)
-                            if "&st=" in repair_page[0]:
-                                repair_st = repair_page[0].split("&st=", 1)
-                                if "&hilite=" in repair_st[0]:
-                                    repair_hilite = repair_st[0].split("&hilite=", 1)
-                                    repair_page[0] = repair_hilite[0]
-                                else:
-                                    repair_page[0] = repair_st[0]
-                            if "&hilite=" in name[0]:
-                                repair_hilite = name[0].split("&hilite=", 1)
-                                name[0] = repair_hilite[0]
-                            if caption[0].count("[") == 0:
-                                repaired_text = repair_sits[0] + string_repair + caption[0] + "|" + repair_page[0] + "||ללא|עמוד=" + name[0] + "}}" + caption[1]
-                        else:
-                            name = repair_pdfpager[1].split(" ", 1)
-                            caption = name[1].split("]", 1)
-                            if "&st=" in name[0]:
-                                repair_st = name[0].split("&st=", 1)
-                                if "&hilite=" in repair_st[0]:
-                                    repair_hilite = repair_st[0].split("&hilite=", 1)
-                                    name[0] = repair_hilite[0]
-                                else:
-                                    name[0] = repair_st[0]
-                            if "&hilite=" in name[0]:
-                                repair_hilite = name[0].split("&hilite=", 1)
-                                name[0] = repair_hilite[0]
-                            if caption[0].count("[") == 0:
-                                repaired_text = repair_sits[0] + string_repair + caption[0] + "|" + name[0] + "||ללא}}" + caption[1]
-
-                elif "[http://www.hebrewbooks.org/" in parts:
-                    if not "http://www.hebrewbooks.org/rambam" or "http://www.hebrewbooks.org/root" or "http://www.hebrewbooks.org/shas" in parts:
-                        list1 = parts.split("[http://www.hebrewbooks.org/", 1)
-                        parts2 = list1[1].split(" ", 1)
-                        list_descript_book = parts2[1].split("]", 1)
-                        if list_descript_book[0].count("[") == 0:
-                            repaired_text = list1[0] + string_repair + list_descript_book[0] + "|" + parts2[0] + "||ללא}}" + list_descript_book[1]
-                elif "[http://hebrewbooks.org/" in parts:
-                    if not "http://hebrewbooks.org/rambam" or "http://hebrewbooks.org/root" or "http://hebrewbooks.org/shas" in parts:
-                        list1 = parts.split("[http://hebrewbooks.org/", 1)
+                elif string in parts:
+                    if not string + "rambam" or string + "root" or string + "shas" in parts:
+                        list1 = parts.split(string, 1)
                         parts2 = list1[1].split(" ", 1)
                         list_descript_book = parts2[1].split("]", 1)
                         if list_descript_book[0].count("[") == 0:
                             repaired_text = list1[0] + string_repair + list_descript_book[0] + "|" + parts2[0] + "||ללא}}" + list_descript_book[1]
 
-    if not "{{ללא בוט}}" in parts:
-        return repaired_text
-    else:
+    if "{{ללא בוט}}" in parts:
         return parts
+
+    else:
+        return repaired_text
 
 
 
@@ -194,6 +122,6 @@ string = get_page_text(page_title)
 #print()
 #print("------")
 #print()
-print(repair_text(string, strings))
+print(repair_text(string))
 #print()
 #print("------")
